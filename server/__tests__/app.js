1726a44bf0d8dbf9bbb23571bbb6601764875903
Matt Burstein <j.matthew.burstein@gmail.com>
2018-04-02 00:38:43 +0100

    fix: server test time out fixed

--- server/__tests__/app.js
+++ server/__tests__/app.js
@@ -2,10 +2,10 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('infrastructure setup', () => {
-  it('responds to the GET method', (done => {
-    request(app).get('/').then((res) => {
+  it('responds to the GET method', (done) => {
+    request(app).get('/api').then((res) => {
       expect(res.statusCode).toBe(200);
       done();
     })
-  }))
+  });
 });
--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 app.get('/api', (req, res) => {
   console.log('in here')
-  axios.get('https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnlimit=1&rnnamespace=0')
+  return axios.get('https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnlimit=1&rnnamespace=0')
     .then(response => {
       var article = response.data.query.random[0]
       article.url = `https://en.wikipedia.org/wiki/${article.title}`
@@ -14,9 +14,5 @@ app.get('/api', (req, res) => {
       console.log('error is:', err)
     })
 });
-
-// app.get('/api/hello', (req, res) => {
-//   res.send({ express: "hello from Express" })
-// });
 
 module.exports = app;
