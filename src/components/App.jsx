c30d2862185c73d511d999f51a3bf770a3ed5897
Matt Burstein <j.matthew.burstein@gmail.com>
2018-04-01 13:18:19 +0100

    Add Article to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Article from './article';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h2>Knowledgizer</h2>
+        <Article />
       </div>
     )
   }
--- src/components/__tests__/App.js
+++ src/components/__tests__/App.js
@@ -8,4 +8,10 @@ describe('App', () => {
   it('renders properly', () => {
     expect(app).toMatchSnapshot();
   });
+
+  it('Contains an Article component', () => {
+    const article = app.find('Article');
+
+    expect(article.exists()).toBe(true);
+  })
 });
