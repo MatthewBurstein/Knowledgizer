0fd392a5055918313b00bdac44f0e7e7db43fb74
Matt Burstein <j.matthew.burstein@gmail.com>
2018-04-02 09:48:10 +0100

    init commit on this branch

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -12,12 +12,17 @@ class App extends Component {
       .catch(err => console.log(err));
   };
 
-  callApi = async () => {
-    const response = await fetch('/api');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
+  callApi = () => {
+    return new Promise((res, rej) {
+      fetch('/api')
+        .resolve(response => JSON.parse(response))
+        .reject(err => console.log(err))
+    })
+    // const response = await fetch('/api');
+    // const body = await response.json();
+    // if (response.status !== 200) throw Error(body.message);
 
-    return body;
+    // return body;
   };
 
   render() {
