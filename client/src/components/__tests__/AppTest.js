e510cb6ef47b9e9d06c8de49de2df2883cb4984b
Matt Burstein <j.matthew.burstein@gmail.com>
2018-04-17 12:21:46 +0100

    Test: full test for delayAndRepeat functionality in App.jsx

--- client/src/components/__tests__/AppTest.js
+++ client/src/components/__tests__/AppTest.js
@@ -1,15 +1,16 @@
 import util from 'util';
 import React from 'react';
 import App from '../App';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 jest.useFakeTimers();
 
 describe('App', () => {
   const articles = [
     { id:1, title: 'some title', url: 'https://en.wikipedia.org/wiki/some title'},
-    { id:2, title: 'another title', url: 'https://en.wikipedia.org/wiki/another title'}
-  ]
+    { id:2, title: 'another title', url: 'https://en.wikipedia.org/wiki/another title'},
+    { id: 3, title: 'a third title', url: 'https://en.wikipedia.org/wiki/a third title' }
+  ];
   const app = shallow(<App />);
   app.setState({ articles });
 
@@ -19,6 +20,36 @@ describe('App', () => {
 
   it('Contains an Article component for each article in `state`', () => {
     const articles = app.find('Article');
-    expect(articles).toHaveLength(2);
-  })
+    expect(articles).toHaveLength(3);
+  });
+
+  describe('delay and repeat article rendering functionality', () => {
+    let callApiSpy, didMountSpy, setStateSpy, mountedApp;
+    beforeAll(() => {
+      callApiSpy = jest.spyOn(App.prototype, 'callApi').mockReturnValue('does this work').mockReturnValue(Promise.resolve({articles}));
+      setStateSpy = jest.spyOn(App.prototype, 'setState');
+      mountedApp = mount(<App />);
+    });
+
+    it('calls callApi on mounting', () => {
+      expect(callApiSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('calls setState once per second with the next article from this.articles', () => {
+      expect(setStateSpy).not.toHaveBeenCalled();
+
+      jest.runTimersToTime(1000);
+      expect(setStateSpy).toHaveBeenCalledTimes(1);
+      expect(mountedApp.state().articles).toContain(articles[0]);
+
+      jest.runTimersToTime(1000);
+      expect(setStateSpy).toHaveBeenCalledTimes(2);
+      expect(mountedApp.state().articles).toContain(articles[1]);
+
+      jest.runTimersToTime(1000);
+      expect(setStateSpy).toHaveBeenCalledTimes(3);
+      expect(mountedApp.state().articles).toContain(articles[2]);
+    });
+  });
+
 });
