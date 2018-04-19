352170d794bd834706ebc7c08670f1a714f498b3
Matt Burstein <j.matthew.burstein@gmail.com>
2018-04-19 20:21:52 +0100

    update webpack.config.dev.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -12,15 +12,15 @@ export default {
     publicPath: '/',
     filename: 'bundle.js'
   },
-  "module": {
+  module: {
     rules: [
       {
-        "exclude": "/node_modules/",
-        "loader": "babel-loader",
-        "options": {
-          "presets": ["es2015", "react", "stage-2"]
+        exclude: "/node_modules/",
+        loader: "babel-loader",
+        options: {
+          presets: ["es2015", "react", "stage-2"]
         },
-        "test": /\.jsx?$/
+        test: /\.jsx?$/
       }
     ]
   },
