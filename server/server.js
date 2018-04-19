51b8b5b379c54a3bf7788030304fd6db3115f936
Matt Burstein <j.matthew.burstein@gmail.com>
2018-04-19 15:02:14 +0100

    chore: clean up server file slightly

--- server/server.js
+++ server/server.js
@@ -7,27 +7,25 @@ import axios from 'axios';
 import wdm from 'webpack-dev-middleware'
 import { wikiGetRequest } from './helpers/serverHelpers'
 
-
 const port = process.env.PORT || 3000;
 const app = express();
 const compiler = webpack(config);
-
 const NODE_ENV = process.env.NODE_ENV;
 
-if(NODE_ENV !== 'test') { app.use(wdm(compiler, {
+if(NODE_ENV !== 'test') {
+  app.use(wdm(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath
   }));
-}
+};
 
 app.listen(port, err => {
   if (err) {
-    console.log(err);
-  } else {
-    console.log(`Listening on port ${port}`)
-    if (NODE_ENV !== 'test') {
-      open(`http://localhost:${port}`)
-    }
+    console.log(err)
+  }
+  console.log(`Listening on port ${port}`)
+  if (NODE_ENV !== 'test') {
+    open(`http://localhost:${port}`)
   }
 });
 
